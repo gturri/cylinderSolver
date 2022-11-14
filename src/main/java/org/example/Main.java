420468acdbc14900de676ba114d1891a95f6f728
Guillaume Turri <guillaume.turri@gmail.com>
2022-11-14 21:35:43 +0100

    Fix Main
    it was not compatible with a recent change in CylinderSet

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,29 +3,30 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         CylinderSet set = new CylinderSet(
-                new Cylinder<Integer>(1, 2, 3, 4),
-                new Cylinder<Integer>(2, 3, 4, 1),
-                new Cylinder<Integer>(3, 4, 1, 2),
-                new Cylinder<Integer>(4, 1, 2, 3),
-                new Cylinder<Operation>(Operation.ADD, Operation.MULT, Operation.DIVIDE, Operation.SUB),
-                new Cylinder<Operation>(Operation.MULT, Operation.DIVIDE, Operation.SUB, Operation.ADD),
-                new Cylinder<Operation>(Operation.EQUALS, Operation.EQUALS, Operation.EQUALS, Operation.EQUALS));
+                new Cylinder<Integer>(1, 5, 8, 7),
+                new Cylinder<Integer>(1, 3, 5, 2),
+                new Cylinder<Integer>(0, 2, 9, 2),
+                new Cylinder<Integer>(2, 4, 3, 3),
+                new Cylinder<Operation>(Operation.ADD, Operation.ADD, Operation.SUB, Operation.SUB),
+                new Cylinder<Operation>(Operation.ADD, Operation.MULT, Operation.DIVIDE, Operation.ADD));
 
 
         int nbIterations = 0;
+        int nbSolutions = 0;
         if (set.isValid()) {
+            nbSolutions++;
             System.out.println(set);
         }
         while (set.moveToNextState()) {
             nbIterations++;
-            if (nbIterations % 100000 == 0) {
-                System.out.println(nbIterations);
-            }
             if (set.isValid()) {
+                nbSolutions++;
                 System.out.println(set);
             }
         }
 
+        System.out.println("nb iterations: " + nbIterations);
+        System.out.println("nbSolutions: " + nbSolutions);
         System.out.println("DONE");
     }
 }
